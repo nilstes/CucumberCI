6793aa0088fc34364058fd7718c395144b0d3a04
nilstes <nils.tesdal@gmail.com>
2017-09-21 17:30:39 +0200

    Tatt bort pauser

--- src/test/java/PlasserOrdreTrinnDefinisjoner.java
+++ src/test/java/PlasserOrdreTrinnDefinisjoner.java
@@ -21,25 +21,21 @@ public class PlasserOrdreTrinnDefinisjoner {
     public void at_en_bruker_putter_varer_i_handlekurven(int arg1) throws Throwable {
         driver.get("http://localhost:8080/cuke"); 
        
-        Thread.sleep(2000); // Disse bruker vi kun for å kunne observere testen 
-        WebElement element = driver.findElement(By.id("add-item")); 
+        WebElement element = driver.findElement(By.id("add-item"));
         element.click(); 
-        Thread.sleep(1000); 
-        element = driver.findElement(By.id("go-to-cart")); 
+        element = driver.findElement(By.id("go-to-cart"));
         element.click();
     }
 
     @Når("^brukeren trykker på bestill-knappen$")
     public void brukeren_trykker_på_bestill_knappen() throws Throwable {
-        Thread.sleep(2000); 
         WebElement element = driver.findElement(By.id("place-order"));
         element.click();   
     }
 
     @Så("^skal det opprettes en ordre$")
     public void skal_det_opprettes_en_ordre() throws Throwable {
-        Thread.sleep(2000); 
-        assert(driver.findElement(By.id("order-confirmation")) != null);    
+        assert(driver.findElement(By.id("order-confirmation")) != null);
     }
 
     @Så("^ordren skal legges til i ordrehistorikken$")
@@ -47,7 +43,6 @@ public class PlasserOrdreTrinnDefinisjoner {
         WebElement element = driver.findElement(By.id("view-history")); 
         element.click(); 
         assert(driver.findElement(By.xpath("//li[contains(text(), \"Ordre 3\")]")) != null); 
-        Thread.sleep(2000); 
         driver.close();
     }
 }
